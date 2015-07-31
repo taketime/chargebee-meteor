b0f5c96d9424642883dd07d3608f74c60c0cb92d
Christopher Jones <christopherdavidjones@gmail.com>
2015-07-31 16:31:38 -0700

    Add test that Chargebee includes requestSync function

--- package.js
+++ package.js
@@ -1,20 +1,21 @@
 Package.describe({
-  name: 'taketime:chargebee',
-  version: '0.0.1',
-  summary: 'Chargebee for Meteor',
-  git: 'https://github.com/taketime/chargebee-meteor.git',
-  documentation: 'README.md'
+    name: 'taketime:chargebee',
+    version: '0.0.1',
+    summary: 'Chargebee for Meteor',
+    git: 'https://github.com/taketime/chargebee-meteor.git',
+    documentation: 'README.md'
 });
 
 Npm.depends({ "chargebee": "1.2.5" });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.1.0.2');
-  api.addFiles('chargebee.js', 'server');
+    api.use('underscore');
+    api.versionsFrom('1.1.0.2');
+    api.addFiles('chargebee.js', 'server');
 });
 
 Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('taketime:chargebee');
-  api.addFiles('chargebee-tests.js', 'server');
+    api.use('tinytest');
+    api.use('taketime:chargebee');
+    api.addFiles('chargebee-tests.js', 'server');
 });
