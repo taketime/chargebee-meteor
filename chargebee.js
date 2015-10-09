75f25a6fffe14f3d6d2c498c5fca467d3c55ee44
Christopher Jones <christopherdavidjones@gmail.com>
2015-10-09 00:38:58 -0700

    Pass options to cbee request wrapper; allows use of instance api key, site

--- chargebee.js
+++ chargebee.js
@@ -1,9 +1,16 @@
 this.Chargebee = Npm.require('chargebee');
 
-var runRequest = function(requestWrapper, callback) {
-    requestWrapper.request(function(err, res) {
-        callback(err, res);
-    });
+var runRequest = function(requestWrapper, options, callback) {
+    if (callback === undefined) {
+        callback = options;
+        requestWrapper.request(function(err, res) {
+            callback(err, res);
+        });
+    } else {
+        requestWrapper.request(function(err, res) {
+            callback(err, res);
+        }, options)
+    }
 };
 
 // Include meteor-compatible synchronous request style for a RequestWrapper
